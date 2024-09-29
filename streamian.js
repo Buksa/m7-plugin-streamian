afb837feabe7715487feea3b810c44de200f9261
F0R3V3R50F7 <Josh1092@yandex.com>
2024-09-29 17:46:41 +0100

    5.9 (Release)

--- streamian.js
+++ streamian.js
@@ -337,41 +337,8 @@ function setPageHeader(page, title) {
     if (page.metadata) {
         page.metadata.title = title;
         page.metadata.icon = logo;
-        var currentBg = Plugin.path + "images/bg.png";
-
-        var apiUrl = 'http://ip-api.com/json';  // API to detect user's location
-
-        // Fetch location data from ip-api
-        var response = http.request(apiUrl, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        var locationData = JSON.parse(response);
-
-        // Get the current date using ISO format (YYYY-MM-DDTHH:mm:ss.sssZ)
-        var currentDateString = new Date().toISOString();
-        var month = currentDateString.substring(5, 7); // Extract the month (MM) from the ISO string
-        var day = currentDateString.substring(8, 10);  // Extract the day (DD) from the ISO string
-
-        // Check region and set Halloween or Christmas backgrounds
-        if (locationData.countryCode === 'US' || locationData.countryCode === 'CA' || locationData.countryCode === 'GB') {
-            if (month === '10') {  // October
-                currentBg = Plugin.path + "images/bghalloween.png";
-            } else if (month === '12') {  // December
-                currentBg = Plugin.path + "images/bgxmas.png";
-            }
-        }
-
-        // Set the background for the page
-        page.metadata.background = currentBg;
-
-        // Debugging
-        console.log("[DEBUG] Current background: " + currentBg);
+        page.metadata.background = Plugin.path + "images/bg.png";
     }
-
     page.type = "directory";
     page.contents = "items";
     page.entries = 0;
